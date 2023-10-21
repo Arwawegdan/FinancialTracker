8c2948a7cbaa396082a5888b7e87524e4a35e09a
Arwawegdan <arwa.wegdan2000@gmail.com>
2023-10-21 17:24:45 +0300

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,38 @@
+namespace FinancialTracker;
+
+public class Program
+{
+        public static void Main(string[] args)
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddControllers();
+
+        string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString)
+                                                           .EnableDetailedErrors()
+                                                           .EnableSensitiveDataLogging()
+                                                           .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
+        WebApplication app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+            app.UseWebAssemblyDebugging();
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseBlazorFrameworkFiles();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.MapControllers();
+        app.MapFallbackToFile("index.html");
+
+        app.Run();
+    }
+}
